a4e8ac5c8fe11100786c17123ae1e09ccd6ca42d
Timothée Vital <102168229+onanavitaldev@users.noreply.github.com>
2022-12-24 15:51:42 +0100

    Add files via upload

---
+++ App/App.js
@@ -0,0 +1,11 @@
+/* JavaScript Code */
+
+/* Global variables */
+
+/* Show the date of the actual day */
+function showDate() {}
+showDate();
+
+/* */
+
+/* By ONANA vital - https://onanavitaldev.github.io/me/ */
